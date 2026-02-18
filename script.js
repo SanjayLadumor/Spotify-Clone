7074155f37dd1fbded34fbe46782f4b8f22b04ae
Sanjay Ladumor <sanjayladumor2007@gmail.com>
2026-02-18 09:50:43 +0530

    Update fetch URL to relative path

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getsongs() {
 
-    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let a = await fetch("/songs/")
     let response = await a.text();
     // console.log(response);
     let div = document.createElement("div")
